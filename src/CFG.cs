2cdb5db699687724e58bfcf461dbc7b2ac960f2c
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2023-11-07 19:04:30 +0100

    Publish

---
+++ src/CFG.cs
@@ -0,0 +1,53 @@
+using System.Text.Json;
+
+internal class CFG
+{
+	public static Config config = new();
+
+	public void CheckConfig(string moduleDirectory)
+	{
+		string path = Path.Join(moduleDirectory, "config.json");
+
+		if (!File.Exists(path))
+		{
+			CreateAndWriteFile(path);
+		}
+
+		using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+		using (StreamReader sr = new StreamReader(fs))
+		{
+			// Deserialize the JSON from the file and load the configuration.
+			config = JsonSerializer.Deserialize<Config>(sr.ReadToEnd())!;
+		}
+	}
+
+	private static void CreateAndWriteFile(string path)
+	{
+
+		using (FileStream fs = File.Create(path))
+		{
+			// File is created, and fs will automatically be disposed when the using block exits.
+		}
+
+		Console.WriteLine($"File created: {File.Exists(path)}");
+
+		Config config = new Config
+		{
+			MessageRelay = true,
+			MessageWebhook = "none",
+		};
+
+		// Serialize the config object to JSON and write it to the file.
+		string jsonConfig = JsonSerializer.Serialize(config, new JsonSerializerOptions()
+		{
+			WriteIndented = true
+		});
+		File.WriteAllText(path, jsonConfig);
+	}
+}
+
+internal class Config
+{
+	public bool MessageRelay { get; set; }
+	public string? MessageWebhook { get; set; }
+}
---
+++ src/K4ryuuChatRelay.cs
@@ -0,0 +1,94 @@
+﻿using System.Text;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace K4ryuuDiscordRelay
+{
+	[MinimumApiVersion(5)]
+	public class DiscordRelayPlugin : BasePlugin
+	{
+		public override string ModuleName => "Discord Relay";
+		public override string ModuleVersion => "1.0.0";
+		public override string ModuleAuthor => "K4ryuu";
+		public override void Load(bool hotReload)
+		{
+			new CFG().CheckConfig(ModuleDirectory);
+
+			RegisterEventHandler<EventPlayerChat>((@event, info) =>
+			{
+				if (!CFG.config.MessageRelay)
+					return HookResult.Continue;
+
+				CCSPlayerController player = new CCSPlayerController(NativeAPI.GetEntityFromIndex(@event.Userid));
+
+				if (player == null || !player.IsValid || player.IsBot || @event.Text == null)
+					return HookResult.Continue;
+
+				string team = "ALL";
+				if (@event.Teamonly)
+				{
+					switch ((CsTeam)player.TeamNum)
+					{
+						case CsTeam.Terrorist:
+							{
+								team = "TERRORIST";
+								break;
+							}
+						case CsTeam.CounterTerrorist:
+							{
+								team = "COUNTER-TERRORIST";
+								break;
+							}
+						case CsTeam.Spectator:
+							{
+								team = "SPECTATOR";
+								break;
+							}
+						default:
+							{
+								team = "NONE";
+								break;
+							}
+					}
+				}
+
+				_ = SendWebhookMessage($"{player.PlayerName}<{player.SteamID}>({team}): {@event.Text}");
+
+				return HookResult.Continue;
+			});
+		}
+
+		public async Task SendWebhookMessage(string message)
+		{
+			using (var httpClient = new HttpClient())
+			{
+				var payload = new
+				{
+					content = message
+				};
+
+				var jsonPayload = Newtonsoft.Json.JsonConvert.SerializeObject(payload);
+				var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+				var response = await httpClient.PostAsync(CFG.config.MessageWebhook, content);
+
+				if (!response.IsSuccessStatusCode)
+				{
+					Log($"Failed to send message to Discord webhook. Status code: {response.StatusCode}");
+				}
+			}
+		}
+
+		public void Log(string message)
+		{
+			string logFile = Path.Join(ModuleDirectory, "logs.txt");
+			using (StreamWriter writer = File.AppendText(logFile))
+			{
+				writer.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " " + message);
+			}
+
+			Console.WriteLine(message);
+		}
+	}
+}
